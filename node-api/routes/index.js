dc55cf183496a040a315aed7204b34e56bf60e73
thierno199 <sadiakishk@.com>
2024-01-24 18:22:58 +0100

    update source code of argoc

--- node-api/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userApi = require('./user.api');
-const authApi = require('./auth.api');
-
-router.use('/api/user', userApi);
-router.use('/api/auth', authApi);
-
-module.exports = router;
